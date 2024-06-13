3d576d4f1000dc736a86689a420cbf1befd071c4
Mercy Auma <aumagabriella@gmail.com>
2024-06-13 21:03:52 +0300

    Added a footer section for the ALADA website

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded',
 					this.classList.add('active');
 				});
 		});
-	});
+});
 
 // changing the colour of the navbar as one scrolls
 document.addEventListener('scroll',
@@ -25,4 +25,24 @@ document.addEventListener('scroll',
 		}else{
 			navbar.classList.remove('scrolled');
 		}
-	})
+})
+
+// submitting credentials for the newsletter
+let subscribingForm = document.getElementById('subscribe-form');
+
+subscribingForm.addEventListener('submit', (e) =>{
+	e.preventDefault();
+
+	let fname = document.getElementById('text-input');
+	let email = document.getElementById('email-input');
+
+	if(fname == "" || email == "" ){
+		alert("Ensure you put values in both fields");
+	}else{
+		alert("Your information has successfully been submitted");
+		console.log(`${fname.value} has subscribed to your newsletter under the email: ${email.value}`);
+
+		fname.value = "";
+		email.value = "";
+	}
+})
